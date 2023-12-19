eeebcf1070a59e4cdea53504cb94fc5a78e16620
vladbelk <vladbelk321@gmail.com>
2023-12-19 19:02:26 +0200

    ex2

--- homework/ex2.py
+++ homework/ex2.py
@@ -2,7 +2,7 @@ try:
     n = int(input( "n = "))
     fact = 1
     for i in range(2, n + 1): fact = fact * i
-    print("Факторіл числа", n, " = ", fact)
+    print("Факторіал числа", n, " = ", fact)
 
 
 except Exception as e:
