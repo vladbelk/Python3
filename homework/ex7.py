dfba73663bdbaa15539c76b4aad051d05b6c01cc
vladbelk <vladbelk321@gmail.com>
2023-12-23 12:54:08 +0200

    ex7

---
+++ homework/ex7.py
@@ -0,0 +1,16 @@
+try:
+   start = int(input("Введіть початок діапазону: "))
+   end = int(input("Введіть кінець діапазону: "))
+
+   for i in range(start, end + 1):
+       if i % 3 == 0 and i % 5 == 0:
+           print("Fizz Buzz")
+       elif i % 3 == 0:
+           print("Fizz")
+       elif i % 5 == 0:
+           print("Buzz")
+       else:
+           print(i)
+
+except Exception as e:
+    print(e)
