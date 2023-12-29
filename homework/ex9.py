dbc84b1945b18269e52d643dae8f289fd933acd6
vladbelk <vladbelk321@gmail.com>
2023-12-29 18:31:09 +0200

    ex9

---
+++ homework/ex9.py
@@ -0,0 +1,18 @@
+try:
+    x = 5
+    y = 5
+
+    i = j = 0
+    while i < x:
+        while j < y:
+            if i >= j:
+                print(f' *', end=' ')
+            else:
+                print(f'     ', end=' ')
+            j+=1
+        i+= 1
+        j= 0
+        print()
+
+except Exception as e:
+    print(e)
