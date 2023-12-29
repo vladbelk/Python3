2a6f9e397715a06422ac4fa0a1ce50c4c9d49e89
vladbelk <vladbelk321@gmail.com>
2023-12-29 18:38:45 +0200

    ex13

---
+++ homework/ex13.py
@@ -0,0 +1,13 @@
+area = 9
+mid = area / 2
+
+try:
+    for x in range(area):
+        for y in range(area):
+            # if y >= x : print(f' *', end=' ')
+            # if y <= x: print(f' *', end=' ')
+            if y <= x and y <= area-1 - x: print(f' *', end=' ')
+            else: print(f' ', end=' ')
+        print()
+except Exception as e:
+    print(e)
