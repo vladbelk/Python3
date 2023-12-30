8687a1d822e69d6e78faa909e3015f2e7499f04c
vladbelk <vladbelk321@gmail.com>
2023-12-30 16:14:43 +0200

    ex15

---
+++ homework/ex19.py
@@ -0,0 +1,19 @@
+try:
+    lim = 11
+    x = y = 0
+    while x < lim:
+        while y < lim:
+            # print(x, y, end=' ')
+            if x <= lim - 1 - y and y >=x:
+
+                print(f' *', end=' ')
+            else:
+                print(f'  ', end=' ')
+            y += 1
+
+        x += 1
+        y = 0
+        print()
+
+except Exception as e:
+    prin
